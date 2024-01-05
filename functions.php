5745eb0c3516c4f7320622f76bce8ce703298b95
y.takeyama <y.takeyama@nttcoms.com>
2024-01-05 16:21:53 +0900

    権限対応

--- functions.php
+++ functions.php
@@ -95,3 +95,14 @@ function page_navi() {
 		'next_text' => '<i class="pager__icon fa-solid fa-angle-right"></i>',
 	) );
 }
+
+//特定の権限で管理画面の左メニューを非表示
+function authority_remove_menus(){
+    if ( current_user_can( 'posts-staff') || current_user_can( 'page-staff') ) {
+     remove_menu_page( 'index.php' );
+     remove_menu_page( 'edit-comments.php' );
+     remove_menu_page( 'tools.php' ); 
+     remove_menu_page( 'wpcf7' ); 
+   }
+ }
+ add_action( 'admin_menu', 'authority_remove_menus', 999 );
